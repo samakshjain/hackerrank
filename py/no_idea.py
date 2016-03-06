cab4b07cc3e0cdcc4169af4299e15df50037970d
Samaksh Jain <samakshjain@live.com>
2016-03-06 12:31:48 +0530

    no idea why no_idea didn't had the updated code!

--- py/no_idea.py
+++ py/no_idea.py
@@ -1,1 +1,14 @@
-from sets import set
+n,m  = raw_input().strip().split()
+list_of_integers = [int(i) for i in raw_input().strip().split()]
+list_A = {int(i) for i in raw_input().strip().split()}
+list_B = {int(i) for i in raw_input().strip().split()}
+
+happiness = 0
+
+for i in list_of_integers:
+	if i in list_A:
+		happiness +=1
+	elif i in list_B:
+		happiness -=1
+
+print happiness
